c400afeaa49f6dd49b518e9a8c916ef40d2f8c09
Baruch Medina <54144103+ingbame@users.noreply.github.com>
2022-08-22 23:43:33 -0500

    Actualización de estructura inicial de BD

--- KodiaksInitialStructureSQL.sql
+++ KodiaksInitialStructureSQL.sql
@@ -34,7 +34,11 @@ INSERT INTO [App].[MenuItems]
 	(MenuItemId,Title,IconSource,TargetPage)
 VALUES
 	(1,'Inicio', 'icon_about.png', 'KodiaksApp.Detail'),
-	(2,'Usuarios', 'icon_feed.png', 'KodiaksApp.Views.vwUsuarios')
+	(2,'Usuarios', 'icon_feed.png', 'KodiaksApp.Views.vwUsuarios'),
+	(3,'Finanzas', 'icon_feed.png', 'KodiaksApp.Views.vwFinanzas'),
+	(4,'Roster', 'icon_feed.png', 'KodiaksApp.Views.vwRoster'),
+	(5,'Juegos', 'icon_feed.png', 'KodiaksApp.Views.vwJuegos'),
+	(6,'Asistencia', 'icon_feed.png', 'KodiaksApp.Views.vwAsistencia')
 SET IDENTITY_INSERT [App].[MenuItems] OFF;
 GO
 CREATE TABLE [App].[AssignRoleMenu](
@@ -60,6 +64,7 @@ CREATE TABLE [Sec].[Users](
 	RoleId INT NOT NULL,
 	CanEdit BIT NOT NULL DEFAULT(0),
 	SavePasswords BIT NOT NULL DEFAULT(0),
+	IsVerified BIT NOT NULL DEFAULT(0),
 	IsActive BIT NOT NULL DEFAULT(1),
 	CreatedDate DATETIME NOT NULL DEFAULT (GETUTCDATE()),
 	CONSTRAINT PK_Sec_Users_Id PRIMARY KEY (UserId),
@@ -74,73 +79,158 @@ CREATE TABLE [Sec].[PasswordsHistory](
 	CONSTRAINT FK_Sec_PasswordsHistory_UserId FOREIGN KEY (UserId) REFERENCES [Sec].[Users](UserId)
 )
 GO
-CREATE TABLE [Fina].[PaymentMethods](
-	MethodId INT IDENTITY(1,1),
-	MethodDesc VARCHAR(50) NOT NULL,
-	CONSTRAINT PK_dbo_PaymentMethods_Id PRIMARY KEY (MethodId)
+CREATE TABLE [Stats].[BattingThrowingSides](
+	BTSideId SMALLINT IDENTITY(1,1),
+	KeyValue VARCHAR(5),
+	BTSideDesc VARCHAR(50),
+	CONSTRAINT PK_Stats_BattingThrowingSide_Id PRIMARY KEY (BTSideId)
 )
 GO
-INSERT INTO [Fina].[PaymentMethods]
-	(MethodDesc)
+INSERT INTO [Stats].[BattingThrowingSides]
+	(KeyValue,BTSideDesc)
 VALUES
-	('Transferencia'),
-	('Efectivo')
+	('L/L', 'Left/Left'),
+	('L/R', 'Left/Right'),
+	('R/R', 'Right/Right'),
+	('R/L', 'Right/Left')
 GO
-CREATE TABLE [Fina].[Members](
+CREATE TABLE [App].[Members](
 	MemberId BIGINT IDENTITY(1,1),
 	UserId BIGINT NOT NULL,
 	FullName VARCHAR(150),
 	NickName VARCHAR(50),
+	ShirtNumber INT NOT NULL,
+	BTSideId SMALLINT NOT NULL,
 	PhotoUrl VARCHAR(MAX),
 	Birthday DATE NOT NULL,
 	Email VARCHAR(50) UNIQUE,
 	CellPhoneNumber VARCHAR(15) UNIQUE,
-	CONSTRAINT PK_dbo_Members_Id PRIMARY KEY (MemberId),
-	CONSTRAINT UK_dbo_Members_MemberIdUserId UNIQUE (MemberId,UserId),
-	CONSTRAINT FK_dbo_Members_UserId FOREIGN KEY (UserId) REFERENCES [Sec].[Users](UserId)
+	CONSTRAINT PK_Fina_Members_Id PRIMARY KEY (MemberId),
+	CONSTRAINT UK_Fina_Members_MemberIdUserId UNIQUE (MemberId,UserId),
+	CONSTRAINT FK_Fina_Members_UserId FOREIGN KEY (UserId) REFERENCES [Sec].[Users](UserId),
+	CONSTRAINT FK_Fina_Members_BTSideId FOREIGN KEY (BTSideId) REFERENCES [Stats].[BattingThrowingSides](BTSideId)
 )
 GO
+CREATE TABLE [Stats].[Positions](
+	PositionId SMALLINT IDENTITY(1,1),
+	KeyValue VARCHAR(3),
+	PositionDesc VARCHAR(50),
+	CONSTRAINT PK_Stats_Positions_Id PRIMARY KEY (PositionId)
+)
+GO
+INSERT INTO [Stats].[Positions]
+	(KeyValue,PositionDesc)
+VALUES
+	('LF', 'Left Field'),
+	('CF', 'Center Field'),
+	('RF', 'Right Field'),
+	('SHF', 'Short Hitter Field'),
+	('3B', '3rd Base'),
+	('SS', 'Shortstop'),
+	('2B', '2nd Base'),
+	('1B', '1st Base'),
+	('P', 'Pitcher'),
+	('C', 'Catcher')
+GO
+CREATE TABLE [Stats].[Roster](
+	RosterId BIGINT IDENTITY(1,1),
+	MemberId BIGINT NOT NULL,
+	PositionId SMALLINT NOT NULL,
+	CreatedDate DATETIME NOT NULL DEFAULT(GETUTCDATE()),
+	CreatedBy BIGINT NOT NULL,
+	CONSTRAINT PK_Stats_Roster_Id PRIMARY KEY (RosterId),
+	CONSTRAINT UK_Stats_Roster_MemberIdUserId UNIQUE (MemberId,PositionId),
+	CONSTRAINT FK_Stats_Roster_PositionId FOREIGN KEY (PositionId) REFERENCES [Stats].[Positions](PositionId),
+	CONSTRAINT FK_Stats_Roster_UserId FOREIGN KEY (CreatedBy) REFERENCES [Sec].[Users](UserId)
+)
+GO
+CREATE TABLE [Fina].[PaymentMethods](
+	MethodId SMALLINT IDENTITY(1,1),
+	MethodDesc VARCHAR(50) NOT NULL,
+	CONSTRAINT PK_Fina_PaymentMethods_Id PRIMARY KEY (MethodId)
+)
+GO
+SET IDENTITY_INSERT [Fina].[PaymentMethods] ON; 
+INSERT INTO [Fina].[PaymentMethods]
+	(MethodId,MethodDesc)
+VALUES
+	(1,'Transferencia'),
+	(2,'Efectivo')	
+SET IDENTITY_INSERT [Fina].[PaymentMethods] OFF;
+GO
+CREATE TABLE [Fina].[ConceptTypes](
+	ConceptTypeId SMALLINT IDENTITY(1,1),
+	ConceptTypeKey VARCHAR(3) NOT NULL,
+	ConceptTypeDesc VARCHAR(50) NOT NULL,
+	CONSTRAINT PK_Fina_ConceptTypes_Id PRIMARY KEY (ConceptTypeId),
+	CONSTRAINT UQ_Fina_ConceptTypes_Desc UNIQUE (ConceptTypeDesc),
+	CONSTRAINT UQ_Fina_ConceptTypes_Key UNIQUE (ConceptTypeKey)
+)
+GO
+SET IDENTITY_INSERT [Fina].[ConceptTypes] ON;
+INSERT INTO [Fina].[ConceptTypes]
+	(ConceptTypeId,ConceptTypeKey,ConceptTypeDesc)
+VALUES
+	(1,'ING','Ingreso'),
+	(2,'GTO','Gasto')
+SET IDENTITY_INSERT [Fina].[ConceptTypes] OFF;
+GO
 CREATE TABLE [Fina].[Concepts](
-	ConceptId INT IDENTITY(1,1),
+	ConceptId SMALLINT IDENTITY(1,1),
+	ConceptTypeId SMALLINT NOT NULL,
+	ConceptKey VARCHAR(3) NOT NULL,
 	ConceptDesc VARCHAR(50) NOT NULL,
-	CONSTRAINT PK_dbo_Concepts_Id PRIMARY KEY (ConceptId)
+	CONSTRAINT PK_Fina_Concepts_Id PRIMARY KEY (ConceptId),
+	CONSTRAINT UQ_Fina_Concepts_Type_Key UNIQUE (ConceptTypeId,ConceptKey),
+	CONSTRAINT UQ_Fina_Concepts_Type_Desc UNIQUE (ConceptTypeId,ConceptDesc),
+	CONSTRAINT FK_Fina_Concepts_ConceptTypeId FOREIGN KEY (ConceptTypeId) REFERENCES [Fina].[ConceptTypes](ConceptTypeId)
 )
 GO
 INSERT INTO [Fina].[Concepts]
-	(ConceptDesc)
+	(ConceptTypeId,ConceptKey,ConceptDesc)
 VALUES
-	('Inscripción'),
-	('Ampayeo'),
-	('Uniformes'),
-	('Hidratación'),
-	('Ajuste')
+	(1,'INS','Inscripción'),
+	(1,'AMP','Ampayeo'),
+	(1,'UNI','Uniformes'),
+	(1,'AJT','Ajuste'),
+	(2,'HID','Hidratación'),
+	(2,'INS','Inscripción'),
+	(2,'AMP','Ampayeo'),
+	(2,'UNI','Uniformes')
 GO
-CREATE TABLE [Fina].[Payments](
-	PaymentId BIGINT IDENTITY(1,1),
+CREATE TABLE [Fina].[Income](
+	IncomeId BIGINT IDENTITY(1,1),
 	MemberId BIGINT NOT NULL,
-    PaymentDate DATETIME NOT NULL,
-	ConceptId INT NOT NULL,
-    MethodId INT NOT NULL,
+	ConceptId SMALLINT NOT NULL,
+	MethodId SMALLINT NOT NULL,
+    IncomeDate DATETIME NOT NULL,
 	Amount DECIMAL(16,2) NOT NULL,
 	AdditionalComment VARCHAR(200),
     EvidenceUrl VARCHAR(MAX),
-	CONSTRAINT PK_dbo_Payments_Id PRIMARY KEY (PaymentId),
-	CONSTRAINT FK_dbo_Payments_MethodId FOREIGN KEY (MethodId) REFERENCES [dbo].[PaymentMethods](MethodId),
-	CONSTRAINT FK_dbo_Payments_MemberId FOREIGN KEY (MemberId) REFERENCES [dbo].[Members](MemberId),
-	CONSTRAINT FK_dbo_Payments_ConceptId FOREIGN KEY (ConceptId) REFERENCES [dbo].[Concepts](ConceptId)
+	CreatedDate DATETIME NOT NULL DEFAULT(GETUTCDATE()),
+	CreatedBy BIGINT NOT NULL,
+	CONSTRAINT PK_Fina_Income_Id PRIMARY KEY (IncomeId),
+	CONSTRAINT FK_Fina_Income_MemberId FOREIGN KEY (MemberId) REFERENCES [App].[Members](MemberId),
+	CONSTRAINT FK_Fina_Income_ConceptId FOREIGN KEY (ConceptId) REFERENCES [Fina].[Concepts](ConceptId),
+	CONSTRAINT FK_Fina_Income_MethodId FOREIGN KEY (MethodId) REFERENCES [Fina].[PaymentMethods](MethodId),
+	CONSTRAINT FK_Fina_Income_UserId FOREIGN KEY (CreatedBy) REFERENCES [Sec].[Users](UserId)
 )
 GO
 CREATE TABLE [Fina].[Bills](
 	BillId BIGINT IDENTITY(1,1),
 	MemberId BIGINT NOT NULL,
-    PaymentDate DATETIME NOT NULL,
-	ConceptId INT NOT NULL,
-    MethodId INT NOT NULL,
+	ConceptId SMALLINT NOT NULL,
+	MethodId SMALLINT NOT NULL,
+    IncomeDate DATETIME NOT NULL,
 	Amount DECIMAL(16,2) NOT NULL,
+	AdditionalComment VARCHAR(200),
     EvidenceUrl VARCHAR(MAX),
-	CONSTRAINT PK_dbo_Payments_Id PRIMARY KEY (BillId),
-	CONSTRAINT FK_dbo_Payments_MethodId FOREIGN KEY (MethodId) REFERENCES [dbo].[PaymentMethods](MethodId),
-	CONSTRAINT FK_dbo_Payments_MemberId FOREIGN KEY (MemberId) REFERENCES [dbo].[Members](MemberId),
-	CONSTRAINT FK_dbo_Payments_ConceptId FOREIGN KEY (ConceptId) REFERENCES [dbo].[Concepts](ConceptId)
+	CreatedDate DATETIME NOT NULL DEFAULT(GETUTCDATE()),
+	CreatedBy BIGINT NOT NULL,
+	CONSTRAINT PK_Fina_Bills_Id PRIMARY KEY (BillId),
+	CONSTRAINT FK_Fina_Bills_MemberId FOREIGN KEY (MemberId) REFERENCES [App].[Members](MemberId),
+	CONSTRAINT FK_Fina_Bills_ConceptId FOREIGN KEY (ConceptId) REFERENCES [Fina].[Concepts](ConceptId),
+	CONSTRAINT FK_Fina_Bills_MethodId FOREIGN KEY (MethodId) REFERENCES [Fina].[PaymentMethods](MethodId),
+	CONSTRAINT FK_Fina_Bills_UserId FOREIGN KEY (CreatedBy) REFERENCES [Sec].[Users](UserId)
 )
 GO
---
+++ KodiaksInitialStructureSQL_Rollback.sql
@@ -0,0 +1,36 @@
+DROP TABLE [Fina].[Bills]
+GO
+DROP TABLE [Fina].[Income]
+GO
+DROP TABLE [Fina].[Concepts]
+GO
+DROP TABLE [Fina].[ConceptTypes]
+GO
+DROP TABLE [Fina].[PaymentMethods]
+GO
+DROP TABLE [Stats].[Roster]
+GO
+DROP TABLE [Stats].[Positions]
+GO
+DROP TABLE [App].[Members]
+GO
+DROP TABLE [Stats].[BattingThrowingSides]
+GO
+DROP TABLE [Sec].[PasswordsHistory]
+GO
+DROP TABLE [Sec].[Users]
+GO
+DROP TABLE [App].[AssignRoleMenu]
+GO
+DROP TABLE [App].[MenuItems]
+GO
+DROP TABLE [App].[Roles]
+GO
+DROP SCHEMA [Sec]
+GO
+DROP SCHEMA [App]
+GO
+DROP SCHEMA [Fina]
+GO
+DROP SCHEMA [Stats]
+GO
